e76eee8e0e85e757c97bee9b835c41ca18a84752
baddev <38311102+qbaddev@users.noreply.github.com>
2020-07-12 21:38:44 +0300

    опубликовано

---
+++ bin2array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bin2array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = "byte bin[] = { ";
+
+            byte[] bin = File.ReadAllBytes(args[0]);
+            foreach(var b in bin)
+            {
+                arr += "0x";
+                arr += b.ToString("X") + ", ";
+                //Console.Write(b.ToString("X") + " ");
+            }
+
+            arr += "};";
+
+            string fname = args[0] + ".c";
+
+            File.WriteAllText(fname, arr);
+            Console.WriteLine("OK! Создан массив из " + bin.Length.ToString() + " байт. Результат записан в файл " + fname);
+        }
+    }
+}
