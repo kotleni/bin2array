e1d221e59cfa3daeabe94b5ae5451b3748eadd20
Victor Varenik <38311102+kotleni@users.noreply.github.com>
2020-12-07 00:42:01 +0200

    Update Program.cs

--- bin2array/Program.cs
+++ bin2array/Program.cs
@@ -13,19 +13,18 @@ namespace bin2array
         {
             string arr = "byte bin[] = { ";
 
-            byte[] bin = File.ReadAllBytes(args[0]);
-            foreach(var b in bin)
+            byte[] bin = File.ReadAllBytes(args[0]); // прочитать файл в массив байтов
+            foreach(var b in bin) // проходимся по всем байтам
             {
-                arr += "0x";
+                arr += "0x"; 
                 arr += b.ToString("X") + ", ";
-                //Console.Write(b.ToString("X") + " ");
             }
 
             arr += "};";
 
-            string fname = args[0] + ".c";
+            string fname = args[0] + ".c"; // будущее имя файла
 
-            File.WriteAllText(fname, arr);
+            File.WriteAllText(fname, arr); // записываем результат в файл
             Console.WriteLine("OK! Создан массив из " + bin.Length.ToString() + " байт. Результат записан в файл " + fname);
         }
     }
